e9db9fdcf83d85bb4e9a03d221a73bf2111b2dd1
gradan-hash <mutukunyaa@gmail.com>
2024-02-19 19:09:02 +0300

    best

--- src/components/ProvidersDashboard/ProviderMessage.jsx
+++ src/components/ProvidersDashboard/ProviderMessage.jsx
@@ -7,30 +7,41 @@ const ProviderMessage = () => {
   const [messages, setMessages] = useState([]);
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-  console.log(currentUser._id);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await newRequests(
-          `/providermesages/${currentUser._id}`
+          `/providermessages/${currentUser._id}`
         );
         console.log(response.data);
-        setMessages(response.data);
+
+        // Assuming you want to filter by the uniqueId of the first message
+        // Check if there are any messages and get the uniqueId of the first message
+        const firstMessageUniqueId =
+          response.data.length > 0 ? response.data[0].uniqueid : null;
+
+        // Filter messages to only include those with the same uniqueId as the first message
+        const filteredMessages = firstMessageUniqueId
+          ? response.data.filter(
+              (message) => message.uniqueid === firstMessageUniqueId
+            )
+          : [];
+
+        setMessages(filteredMessages);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [currentUser._id]); // Added dependency to re-fetch if the currentUser._id changes
 
   return (
     <>
       <Sidebar />
 
       <div>
-        {messages?.map((message) => (
+        {messages.map((message) => (
           <div key={message._id} className="message">
             <p>{message.message}</p>
             <p>From: {message.sender}</p>
