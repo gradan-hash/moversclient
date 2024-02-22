a764b4eba7f569a68bb4bee067d802c32df6d06f
gradan-hash <mutukunyaa@gmail.com>
2024-02-22 10:34:31 +0300

    choice

--- src/components/Orders/OngoingOrders.jsx
+++ src/components/Orders/OngoingOrders.jsx
@@ -32,22 +32,22 @@ const OngoingOrders = () => {
               <th>Service Needed</th>
               <th>Service Provider</th>
               <th>{`Reg. Number / Warehouse`}</th>
-            
+
               <th>Location</th>
               <th>Price</th>
             </tr>
           </thead>
           <tbody>
-            {orders.map((order,index) => (
+            {orders.map((order, index) => (
               <tr key={index}>
                 <td>{order._id}</td>
                 <td>{order.userdetails.username}</td>
                 <td>{order.itemdetails.serviceType}</td>
                 <td>{order.providerdetails.companyname}</td>
                 <td>{order.itemdetails.cartype}</td>
-              
-                <td>{order.location}</td>
-                <td>{order.price}</td>
+
+                <td>{order.itemdetails.operationLocation}</td>
+                <td>{order.itemdetails.quotation}</td>
               </tr>
             ))}
           </tbody>
